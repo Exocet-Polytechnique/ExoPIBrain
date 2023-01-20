c372ecaaca18845ab98db301ce83aa1d0deb7087
Étienne <elescarbeault@gmail.com>
2023-01-20 15:39:23 -0500

    start of skeleton

---
+++ multithreading/sensor_reader.py
@@ -0,0 +1,15 @@
+import threading
+import time
+import logging
+
+class MessagePrinter(threading.Thread):
+    def __init__(self, *args, **kwargs):
+        threading.Thread.__init__(self)
+        self._args = args
+        self._kwargs = kwargs
+        self._lock = kwargs.get("lock", None)
+
+    def run(self):
+        for message in self._args:
+            logging.info(message)
+            time.sleep(self._kwargs.get("delay", 1.0))
