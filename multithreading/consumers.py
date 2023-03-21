4f88d0bcdd26e278d65827bc841b453ce0af1b8d
Étienne <elescarbeault@gmail.com>
2023-03-21 13:22:08 -0400

    added def for lig consumer

--- multithreading/consumers.py
+++ multithreading/consumers.py
@@ -1,5 +1,6 @@
 import threading
 from asserts.checks import get_check
+import serial
 
 class Consumer(threading.Thread):
     def __init__(self,lock, queue):
@@ -10,19 +11,33 @@ class Consumer(threading.Thread):
     def run(self):
         pass
 class DataConsumer(Consumer):
-    def __init__(self,lock, queue):
+    def __init__(self,lock, queue, serial_port):
         super().__init__(lock, queue)
-    
+        
+
     def run(self):
         while True:
             name, data = self.queue.get()[1]
             get_check(name)(data)
             print(data)
             self.queue.task_done()
-class DisplayConsumer(Consumer):
-    def __init__(self,lock, display_queue):
-        super().__init__(lock, display_queue)
-    
-    def run(self):
+
+class LogConsumer(Consumer):
+    def __init__(self,lock, log_queue, serial_port):
+        super().__init__(lock, log_queue)
+        self.serial_port = serial_port
+        self.serial = serial.Serial(self.serial_port)
+
+    def write_telemetry(self, data):
         pass
+
+    def write_screen(self, data):
+        pass
+
+    def run(self):
+        # TODO: Emit to the arduino and display on the screen
+        data = self.queue.get()
+        self.write_telemetry(data)
+        self.write_screen(data)
+        self.queue.task_done()
 
