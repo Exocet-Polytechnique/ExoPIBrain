e35f0a594ab25cfdea8d1861c55200aeba60889f
Étienne <elescarbeault@gmail.com>
2023-05-16 21:19:21 -0400

    added icons in ui

--- exopibrain/display/ui.py
+++ exopibrain/display/ui.py
@@ -1,12 +1,9 @@
 import sys
 import qtawesome as qta
 
-from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout
-class DataWidget(QtWidgets.QLabel):
-    def __init__(self, text):
-        super().__init__(text=text)
-        self.setFont(QtGui.QFont("Arial", 50))
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout
+
     
 
 class TimeWidget(QtWidgets.QLabel):
@@ -26,24 +23,64 @@ class TimeWidget(QtWidgets.QLabel):
         label_time = current_time.toString('hh:mm:ss')
         # showing it to the label
         self.setText(label_time)
+
+class DataWidget(QtWidgets.QWidget):
+
+    IconSize = QtCore.QSize(96, 96)
+    HorizontalSpacing = 2
+
+
+    def __init__(self, qta_id, value=0, unit="", final_stretch=True):
+        super(QtWidgets.QWidget, self).__init__()
+        self.qta_id = qta_id
+        self.value = value
+        self.unit = unit
+        self.setFont(QtGui.QFont("Arial", 50))
+
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+        icon = QtWidgets.QLabel()
+        icon.setPixmap(qta.icon(self.qta_id).pixmap(self.IconSize))
+
+        layout.addWidget(icon)
+        layout.addSpacing(self.HorizontalSpacing)
+        self.data_label = QtWidgets.QLabel(f"{value} {unit}")
+        layout.addWidget(self.data_label)
+
+        if final_stretch:
+            layout.addStretch()
+
+    def update(self, new_value):
+        self.value = new_value
+        self.data_label.setText(f"{self.value} {self.unit}")
+
+class TemperatureWidget(DataWidget):
+    def __init__(self, qta_id, value=0, unit="", final_stretch=True):
+        super().__init__(qta_id, value, unit, final_stretch)
+
+    def update(self, new_value):
+        self.value = new_value
+        self.data_label.setText(f"{self.value} {self.unit}")
 class MyWidget(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.time = TimeWidget()
 
-        self.speed = DataWidget("Vitesse")
+        self.speed = DataWidget("fa5s.tachometer-alt", unit="km/h")
 
-        self.temp = DataWidget("Temperature")
+        self.temp = DataWidget("fa5s.thermometer-half", unit="°C")
 
-        self.autonomy = DataWidget("Autonomie")
+        self.autonomy = DataWidget("fa5s.battery-half", unit="km")
 
-        self.pressure = DataWidget("Pression")
+        self.pressure = DataWidget("fa5s.tachometer-alt", unit="bar")
 
-        self.consummation = DataWidget("Consommation")
+        self.consumption = DataWidget("fa5s.tachometer-alt", unit="L")
         
-        self.tank = DataWidget("H2 restant")
-        
+        self.tank = DataWidget("fa5s.tachometer-alt", unit="L")
+
         layout1 = QGridLayout()
         layout2 = QHBoxLayout()
         layout3 = QHBoxLayout()
@@ -57,7 +94,7 @@ class MyWidget(QtWidgets.QMainWindow):
         layout2.addWidget(self.autonomy)
         layout1.addLayout(layout2, 1,0)
         layout3.addWidget(self.pressure)
-        layout3.addWidget(self.consummation)
+        layout3.addWidget(self.consumption)
         layout3.addWidget(self.tank)
         layout1.addLayout(layout3,2,0)
 
