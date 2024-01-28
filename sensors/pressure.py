a7b83be7bcabb9821c70a13dbb3db38eb740e38b
efondere <97473864+efondere@users.noreply.github.com>
2024-01-28 14:35:17 -0500

    Update pressure class

--- sensors/pressure.py
+++ sensors/pressure.py
@@ -1,4 +1,9 @@
 from multithreading.stream_reader import StreamReader
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
 class Manometer(StreamReader):
     """
@@ -8,4 +13,14 @@ class Manometer(StreamReader):
     """
     def __init__(self, lock, data_queue, log_queue, config):
         super().__init__(lock, data_queue, log_queue, config)
-        
+        self.spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+        self.cs = digitalio.DigitalInOut(board.D25)
+        self.mcp = MCP.MCP3008(self.spi, self.cs)
+        self.chan = AnalogIn(self.mcp, MCP.P0)
+
+    def _convert_to_pressure(channel_value):
+        # TODO
+        return channel_value
+
+    def read_raw_data(self):
+        return self._convert_to_pressure(self.chan.value)
