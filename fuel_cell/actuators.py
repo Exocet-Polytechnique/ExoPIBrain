2d73dacdb5dc527b277db745cd02e0784574afd5
Kilian PAYAN <kilianpayan@gmail.com>
2024-01-28 15:56:52 -0500

    Implementation du controle des actuateurs

--- fuel_cell/actuators.py
+++ fuel_cell/actuators.py
@@ -1,10 +1,18 @@
 """
+Controls the valve's actuator
 """
+import RPi.GPIO as GPIO # works in the raspberrypi
 
 
-class Actuator(object):
+class Actuator():
+    def __init__(self, pinNumber):
+        GPIO.setmode(GPIO.BCM)
+        self.pinNumber = pinNumber
+        GPIO.setup(self.pinNumber, GPIO.OUT) # puts the pin pinNumber as an Output
+    
     def open_valve(self):
-        pass
+        GPIO.output(self.pinNumber, GPIO.HIGH) #puts a 0 in the pin, witch open the valve
 
+    
     def close_valve(self):
-        pass
+        GPIO.output(self.pinNumber, GPIO.LOW) # puts a 1 in the pin, witch close the valve
