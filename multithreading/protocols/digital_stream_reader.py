7d21018e483b90a39988b511b2adf01b198ec5cd
efondere <97473864+efondere@users.noreply.github.com>
2024-02-13 14:15:29 -0500

    Simplify `DigitalStreamReader` config

--- multithreading/protocols/digital_stream_reader.py
+++ multithreading/protocols/digital_stream_reader.py
@@ -4,29 +4,22 @@ import RPi.GPIO as GPIO
 class DigitalStreamReader(StreamReader):
     def __init__(self, lock, data_queue, log_queue, config):
         """
-        This class is used to read input from one (or more) digital GPIO pins
+        This class is used to read input from a GPIO pin
 
         Args:
             lock (threading.Lock): The lock used to synchronize access to the queue.
             data_queue (queue.PriorityQueue): The queue to put the data in.
             log_queue (queue.Queue): The queue to put the data in.
-            config (dict): The configuration for the sensor (I2C device id).
+            config (dict): The configuration containing the pin to use.
         """
         super().__init__(lock, data_queue, log_queue, config)
         GPIO.setmode(GPIO.BCM)
-        if "pin" in config:
-            self._pins = [config["pin"]]
-        elif "pins" in config:
-            self._pins = config["pins"]
-        else:
-            raise ValueError("No pin or pins specified in the configuration.")
-
-        # the library allows setting multiple pins at once by passing in a list
-        GPIO.setup(self._pins, GPIO.IN)
+        self._pin = config["pin"]
+        GPIO.setup(self._pin, GPIO.IN)
 
     def join(self):
         """
-        Join the thread and cleanup used pins
+        Join the thread and cleanup used pin
         """
         super().join()
         GPIO.cleanup(self._pins)
