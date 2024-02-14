461a0cf5d3fb15dea386e8bcdd1533cb14fddc42
efondere <97473864+efondere@users.noreply.github.com>
2024-02-14 13:25:04 -0500

    Read temperature sensors in a sequence

--- sensors/temperature.py
+++ sensors/temperature.py
@@ -1,7 +1,6 @@
 from multithreading.stream_reader import StreamReader
 import os
 import time
-import glob
 
 
 # From: https://randomnerdtutorials.com/raspberry-pi-ds18b20-python/
@@ -18,49 +17,61 @@ class Thermocouples(StreamReader):
         super().__init__(lock, data_queue, log_queue, config)
 
         self._values = {name: float("nan") for name in config["sensors"].keys()}
+        self._names = list(config["sensors"].keys())
+        addresses = [config["sensors"][name]["address"] for name in config["sensors"].keys()]
+        self._device_files = [f"/sys/bus/w1/devices/28-{address:012x}/w1_slave" for address in addresses]
         self._current_sensor_index = 0
 
         # Thermocouple data wire should be linked to GPIO4 (see boot/config.txt, last line)
         os.system('modprobe w1-gpio')
         os.system('modprobe w1-therm')
-        base_dir = '/sys/bus/w1/devices/'
-        device_dirs = glob.glob(base_dir + '28*')[0]
-        self.device_file = device_folder + '/w1_slave'
-
-    def _read_lines(self):
-        """
-        Reads the lines from the thermocouple file.
-
-        Returns:
-            list: The lines from the thermocouple file.
-        """
-        with open(self.device_file, 'r') as f:
-            lines = f.readlines()
-        return lines
 
     def _read_current_sensor(self):
+        """
+        Read the temperature data from the current sensor.
+
+        Returns:
+            False if the sensor is not connected
+            True otherwise
+        """
+        name = self._names[self._current_sensor_index]
+        device_file = self._device_files[self._current_sensor_index]
+
+        if not os.path.isfile(device_file):
+            self._current_sensor_index += 1
+            print(f"Warning: the temperature sensor \"{name}\" is not connected.")
+            return False
+
+        with open(self._device_files[self._current_sensor_index], 'r') as f:
+            lines = f.readlines()
+
+        if lines[0].strip()[-3:] != 'YES':
+            self._current_sensor_index += 1
+            print(f"Warning: invalid data received from \"{name}\" temperature sensor.")
+            return True
+
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            self._values[name] = float(temp_string) / 1000.0
+
         self._current_sensor_index += 1
+        return True
 
     def read_raw_data(self):
         """
-        Reads the raw data from the thermocouple with
-        correct formatting.
+        Reads the raw data from the thermocouple with correct formatting.
 
         Returns:
             float: The raw temperature data from the thermocouple.
         """
-        temp_c = None
-        lines = self._read_lines()
-        while lines[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
-            lines = self._read_lines()
-        equals_pos = lines[1].find('t=')
-        if equals_pos != -1:
-            temp_string = lines[1][equals_pos+2:]
-            temp_c = float(temp_string) / 1000.0
-        return temp_c
+        read_successful = self._read_current_sensor()
+        read_counter = 5 # prevent being stuck in an infinite loop
+        while not read_successful and read_counter > 0:
+            read_counter -= 1
+            read_successful = self._read_current_sensor()
 
-        return self.__values
+        return self._values
 
 if __name__ == "__main__":
     import time
