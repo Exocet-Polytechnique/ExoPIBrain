a6198e8057ea651c2e94175ce693c0baa6f23026
efondere <97473864+efondere@users.noreply.github.com>
2024-02-20 23:44:41 -0500

    Update compass class with new stream reader

--- sensors/imu/hmc5883l.py
+++ sensors/imu/hmc5883l.py
@@ -1,7 +1,8 @@
-from sensors.imu.imu_sensor import IMUSensor
+from multithreading.protocols.smbus_stream_reader import SMBusStreamReader
 
 
-class QMC5883(IMUSensor):
+class HMC5883L(SMBusStreamReader):
+    # HMC5883L constants
     RANGE_2G = 0x00
     BW_10Hz = 0x00
     OS_512 = 0x00
@@ -11,60 +12,51 @@ class QMC5883(IMUSensor):
     REG_XOUT_LSB = 0x00  # Output Data Registers for magnetic sensor.
     REG_YOUT_LSB = 0x02
     REG_ZOUT_LSB = 0x04
-    REG_RST_PERIOD = 0x0b   # SET/RESET Period Register.
+    REG_RST_PERIOD = 0x0B   # SET/RESET Period Register.
 
     # Flags for Status Register #1.
     STAT_DRDY = 0b00000001  # Data Ready.
     STAT_OVL = 0b00000010  # Overflow flag.
     STAT_DOR = 0b00000100  # Data skipped for reading.
 
-    def __init__(self, bus, declination=0.0):
-        super().__init__(bus, address=0x0C)
-        self.declination = declination
-        self._calibration = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+    def __init__(self, lock, data_queue, log_queue, config):
+        super().__init__(lock, data_queue, log_queue, config)
 
-    def connect(self):
+    def try_connect(self):
         # no need to add this to __init__ since the stream_reader class has its is_connected member to False
         # by default and will attempt to connect via the imu class
-        try:
-            self.write_byte(self.REG_CONTROL_1, 0x00)
-            self.write_byte(self.REG_CONTROL_2, 0x4D)
-        except:
-            return False
+        with self.acquire_lock():
+            try:
+                self.write_byte(self.REG_CONTROL_1, 0x00)
+                self.write_byte(self.REG_CONTROL_2, 0x4D)
 
-        return True
+            except:
+                return False
+
+            return True
 
 
-    def _read_word(self, registry):
+    def _read_word(self, register):
         """Read a two bytes value stored as LSB and MSB."""
-        low = self.read_byte(registry)
-        high = self.read_byte(registry + 1)
+        low = self.read_byte(register)
+        high = self.read_byte(register + 1)
+
         val = (high << 8) + low
         return val
 
-    def _read_word_2c(self, registry):
+    def _read_word_2c(self, register):
         """Calculate the 2's complement of a two bytes value."""
-        val = self._read_word(registry)
+        val = self._read_word(register)
         if val >= 0x8000:  # 32768
             return val - 0x10000  # 65536
         else:
             return val
 
-    def set_calibration(self, value):
-        """Set the 3x3 matrix for horizontal (x, y) magnetic vector calibration."""
-        c = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
-        for i in range(0, 3):
-            for j in range(0, 3):
-                c[i][j] = float(value[i][j])
-        self._calibration = c
-
-
-    def read(self):
+    def read_raw_data(self):
         """Read data from magnetic and temperature data registers."""
-        x, y, z = None, None, None
-        
-        x = self._read_word_2c(self.REG_XOUT_LSB)
-        y = self._read_word_2c(self.REG_YOUT_LSB)
-        z = self._read_word_2c(self.REG_ZOUT_LSB)
+        with self.acquire_lock():
+            x = self._read_word_2c(self.REG_XOUT_LSB)
+            y = self._read_word_2c(self.REG_YOUT_LSB)
+            z = self._read_word_2c(self.REG_ZOUT_LSB)
         
         return x, y, z
