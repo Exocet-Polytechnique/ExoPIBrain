2d3900876b1a10615940827e85550046482433c9
efondere <97473864+efondere@users.noreply.github.com>
2024-02-20 23:20:50 -0500

    Update Accelerometer class to use new stream reader
    Change read method of Accelerometer

--- sensors/imu/adxl345.py
+++ sensors/imu/adxl345.py
@@ -1,15 +1,16 @@
 # ADXL345 Python library for Raspberry Pi
 #
-# author:  Jonathan Williamson
+# authors:  Jonathan Williamson and Exocet Polymtl
 # license: BSD, see LICENSE.txt included in this package
 #
 # This is a Raspberry Pi Python implementation to help you get started with
 # the Adafruit Triple Axis ADXL345 breakout board:
 # http://shop.pimoroni.com/products/adafruit-triple-axis-accelerometer
 
-from sensors.imu.imu_sensor import IMUSensor
+from multithreading.protocols.smbus_stream_reader import SMBusStreamReader
 
-class ADXL345(IMUSensor):
+
+class ADXL345(SMBusStreamReader):
     # ADXL345 constants
     DATA_FORMAT = 0x31
     BW_RATE = 0x2C
@@ -21,38 +22,41 @@ class ADXL345(IMUSensor):
     SCALE_MULTIPLIER = 0.004
     AXES_DATA = 0x32
 
-    def __init__(self, bus):
-        super().__init__(bus, address=0x53)
+    def __init__(self, lock, data_queue, log_queue, config):
+        super().__init__(lock, data_queue, log_queue, config)
 
-    def connect(self, bus):
+    def try_connect(self):
         # no need to add this to __init__ since the stream_reader class has its is_connected member to False
         # by default and will attempt to connect via the imu class
-        try:
-            self.write_byte(self.BW_RATE, self.BW_RATE_100HZ)
-            self.set_range(self.RANGE_2G)
-            self.write_byte(self.POWER_CTL, self.MEASURE)
-        except:
-            # failed to connect
-            return False
+        with self.acquire_lock():
+            try:
+                self.write_byte(self.BW_RATE, self.BW_RATE_100HZ)
+                self._set_range(self.RANGE_2G)
+                self.write_byte(self.POWER_CTL, self.MEASURE)
 
-        return True
+            except:
+                # failed to connect
+                return False
 
-    def set_range(self, range_flag):
-        value = self.bus.read_byte_data(self.address, self.DATA_FORMAT)
+            return True
+
+    def _set_range(self, range_flag):
+        value = self.read_byte(self.DATA_FORMAT)
         value &= ~0x0F
         value |= range_flag
         value |= 0x08
         self.write_byte(self.DATA_FORMAT, value)
 
-    def read(self, gforce=False):
+    def read_raw_data(self):
         """
         returns the current reading from the sensor for each axis
 
-        parameter gforce:
+        parameter use_gforce_units:
            False (default): result is returned in m/s^2
            True     : result is returned in gs
         """
-        i2c_bytes = self.bus.read_i2c_block_data(self.address, self.AXES_DATA, 6)
+        with self.acquire_lock():
+            i2c_bytes = self.read_block(self.AXES_DATA, 6)
 
         x = i2c_bytes[0] | (i2c_bytes[1] << 8)
         if x & (1 << 16 - 1):
@@ -70,9 +74,8 @@ class ADXL345(IMUSensor):
         y = y * self.SCALE_MULTIPLIER
         z = z * self.SCALE_MULTIPLIER
 
-        if gforce == False:
-            x = x * self.EARTH_GRAVITY_MS2
-            y = y * self.EARTH_GRAVITY_MS2
-            z = z * self.EARTH_GRAVITY_MS2
+        x = x * self.EARTH_GRAVITY_MS2
+        y = y * self.EARTH_GRAVITY_MS2
+        z = z * self.EARTH_GRAVITY_MS2
 
         return x, y, z
