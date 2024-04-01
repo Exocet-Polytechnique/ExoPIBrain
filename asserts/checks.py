39cc99225e885f31c0a8f102d4d32b2decb2bad4
efondere <97473864+efondere@users.noreply.github.com>
2024-04-01 10:57:18 -0400

    Use `SensorResult` in checks

--- asserts/checks.py
+++ asserts/checks.py
@@ -1,25 +1,32 @@
-from asserts.asserts import WarningError, CriticalError
+"""
+Check functions called when processing the data queue.
+"""
+
 from config import CONFIG
+from procedures.exception_handling.sensor_result import SensorResult
+from procedures.exception_handling.messages import create_message_id, WARNING_TEMPERATURE, \
+    CRITICAL_TEMPERATURE, BATTERY_12V_CRITICAL_CHARGE, BATTERY_12V_WARNING_CHARGE, \
+    BATTERY_24V_CRITICAL_CHARGE, BATTERY_24V_WARNING_CHARGE
 
 
 def check_device_temperature(name, temperature, max_temperature_warn, max_temperature_alert):
     """
     Checks the temperature of a sensor.
     """
-    msg = f"{name}: {temperature}."
-
     if max_temperature_warn <= temperature < max_temperature_alert:
-        raise WarningError(msg)
+        return SensorResult(False, create_message_id(name, WARNING_TEMPERATURE))
     if temperature > max_temperature_alert:
-        raise CriticalError(msg)
-    
+        return SensorResult(False, create_message_id(name, CRITICAL_TEMPERATURE))
+
+    return SensorResult(True, None)
+
 
 def check_cpu_temperature(data):
     """
     Checks the CPU temperature.
     """
     temperature = data["temperature"]
-    check_device_temperature(
+    return check_device_temperature(
         CONFIG["RASPBERRY_PI_CPU_TEMPERATURE"]["name"],
         temperature,
         CONFIG["RASPBERRY_PI_CPU_TEMPERATURE"]["warning_temperature"],
@@ -31,38 +38,45 @@ def check_temperatures(data):
     """
     Check temperature from all thermocouples.
     """
+    results = []
     for name, temperature in data.items():
-        check_device_temperature(
+        results.append(check_device_temperature(
             name,
             temperature,
             CONFIG["TEMPERATURES"]["sensors"][name]["warn"],
             CONFIG["TEMPERATURES"]["sensors"][name]["alert"]
-        )
+        ))
+
+    return results
 
 def check_battery_levels(data):
     """
     Checks the charge level of the batteries
     """
+    results = []
+
     battery_charge_12V = data["12V"]["charge_level"]
 
     # This will ultimately be changed when we implement proper error checking and protocols.
     # We should also check the batteries individually, but this will do for now.
     if battery_charge_12V >= CONFIG["BATTERY_GAUGES"]["charge_levels"]["12V"]["alert"]:
-        raise CriticalError("Error: 12V battery charge level is critically low.")
-    if battery_charge_12V >= CONFIG["BATTERY_GAUGES"]["charge_levels"]["12V"]["warning"]:
-        raise WarningError("Warning: 12V battery charge level is low.")
+        results.append(SensorResult(False, BATTERY_12V_CRITICAL_CHARGE))
+    elif battery_charge_12V >= CONFIG["BATTERY_GAUGES"]["charge_levels"]["12V"]["warning"]:
+        results.append(SensorResult(False, BATTERY_12V_WARNING_CHARGE))
 
     battery_charge_24V = data["24V"]["charge_level"]
 
     if battery_charge_24V >= CONFIG["BATTERY_GAUGES"]["charge_levels"]["24V"]["alert"]:
-        raise CriticalError("Error: 24V battery charge level is critically low.")
-    if battery_charge_24V >= CONFIG["BATTERY_GAUGES"]["charge_levels"]["24V"]["warning"]:
-        raise WarningError("Warning: 24V battery charge level is low.")
+        results.append(SensorResult(False, BATTERY_24V_CRITICAL_CHARGE))
+    elif battery_charge_24V >= CONFIG["BATTERY_GAUGES"]["charge_levels"]["24V"]["warning"]:
+        results.append(SensorResult(False, BATTERY_24V_WARNING_CHARGE))
+
+    return results
 
 def check_fuel_cell(data):
     # TODO: figure out something to check. This function is here for redundency, but the fuel cell
     # controllers should already immplement some security.
-    pass
+    return SensorResult(True, None)
 
 
 # List of sensors we want to check as well as their respective functions
@@ -79,5 +93,7 @@ def perform_check(name, data):
     Check a sensor for any problems. This function will raise a `WarningError` or `CriticalError`
     if a problem is detected.
     """
-    if name in _CHECKS.keys():
-        _CHECKS[name](data)
+    if name in _CHECKS:
+        return _CHECKS[name](data)
+
+    return SensorResult(True, None)
