c99279e12daeeea8bd7b93898d8925978ab9fb5d
efondere <97473864+efondere@users.noreply.github.com>
2024-05-02 16:24:34 -0400

    Print pressure

--- tests/temperature_pressure.py
+++ tests/temperature_pressure.py
@@ -1,15 +1,14 @@
 from sensors.temperature import Thermocouples
-# from sensors.pressure import Manometers
+from sensors.pressure import Manometers
 from config import CONFIG
 
 def main():
     """Shows the temperature and pressure"""
     temperature = Thermocouples(None, None, None, CONFIG["TEMPERATURES"])
-    # pressure = Manometers(None, None, None, CONFIG["PRESSURES"])
+    pressure = Manometers(None, None, None, CONFIG["PRESSURES"])
     while True:
         temp_data = temperature.read_raw_data()
-        # pressure_data = pressure.read_raw_data()
-        pressure_data = {"M0": 0, "M1": 0}
+        pressure_data = pressure.read_raw_data()
         print("Plaque H2 (°C):", temp_data["H2_plate"])
         print("Refuelling station (°C):", temp_data["refuelling_station"])
         print("Flottant (°C):", temp_data["floating"])
