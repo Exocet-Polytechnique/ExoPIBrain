5e81a870fc27963c455cd30a76ceb3710470b6a5
gumile <48538620+gumile@users.noreply.github.com>
2024-06-01 14:53:04 -0400

    organized widgets

--- src/interface.rs
+++ src/interface.rs
@@ -6,7 +6,7 @@ use crossterm::{
     ExecutableCommand,
 };
 
-use ratatui::{backend::CrosstermBackend, prelude::*, symbols, widgets::*, Terminal};
+use ratatui::{backend::CrosstermBackend, prelude::*, style::*, symbols, widgets::*, Terminal};
 
 use crate::devices::SensorData;
 
@@ -67,11 +67,37 @@ impl Interface {
                 // Widgets
                 /////////////
                 
-                let battery_widget = Paragraph::new(format!("Capacity: Temperature: {:>6.2}°C\nVoltage:     {:>6.2} V\nCurrent:     {:>6.2} A", 142.1283, 23.21, 5.2))
+                let speed_widget = Paragraph::new(format!("{:>5.2} km/h", 12.1))
                 .style(default_style)
                 .block(
                     Block::default()
                         .borders(Borders::LEFT | Borders::TOP)
+                        .title_style(Style::default().fg(Color::LightMagenta).add_modifier(Modifier::BOLD))
+                        .title(" 󰓅 Speed ")
+                );
+
+                let efficiency_widget = Paragraph::new(format!("{:>5.2} %", 76.324))
+                .style(default_style)
+                .block(
+                    Block::default()
+                        .borders(Borders::LEFT | Borders::TOP)
+                        .border_set(symbols::border::Set {
+                            top_left: symbols::line::NORMAL.vertical_right,
+                            ..symbols::border::PLAIN
+                        })
+                        .title_style(Style::default().fg(Color::LightMagenta).add_modifier(Modifier::BOLD))
+                        .title(" 󰌪 Efficiency ")
+                );
+
+                let battery_widget = Paragraph::new(format!("Capacity:{:>10} %\nTemperature:{:>7.2} °C\nVoltage:{:>11.2} V\nCurrent:{:>11.2} A", 92, 142.1283, 23.21, 5.2))
+                .style(default_style)
+                .block(
+                    Block::default()
+                        .borders(Borders::LEFT | Borders::TOP)
+                        .border_set(symbols::border::Set {
+                            top_left: symbols::line::NORMAL.vertical_right,
+                            ..symbols::border::PLAIN
+                        })
                         .title_style(Style::default().fg(Color::LightGreen).add_modifier(Modifier::BOLD))
                         .title(" 󰁹 Battery ")
                 );
@@ -80,16 +106,25 @@ impl Interface {
                 .style(default_style)
                 .block(
                     Block::default()
-                        .borders(Borders::LEFT | Borders::TOP)
+                        .borders(Borders::LEFT | Borders::TOP | Borders::RIGHT)
+                        .border_set(symbols::border::Set {
+                            top_left: symbols::line::NORMAL.horizontal_down,
+                            ..symbols::border::PLAIN
+                        })
                         .title_style(Style::default().fg(Color::LightBlue).add_modifier(Modifier::BOLD))
                         .title("  Hydrogen ")
                 );
 
-                let fuel_cells_widget = Paragraph::new(format!("Temperature:{:>8.2}  °C\nHigh Pressure:{:>7.2} Bar\nLow Pressure:     {:>6.2} Bar", 142.1283, 23.21, 5.2))
+                let fuel_cells_widget = Paragraph::new(format!("Temperature:{:>8.2} °C\nHigh Pressure:{:>7.2} Bar\nLow Pressure:{:>6.2} Bar", 142.1283, 23.21, 5.2))
                 .style(default_style)
                 .block(
                     Block::default()
-                        .borders(Borders::LEFT | Borders::TOP)
+                        .borders(Borders::LEFT | Borders::TOP | Borders::RIGHT)
+                        .border_set(symbols::border::Set {
+                            top_left: symbols::line::NORMAL.cross,
+                            top_right: symbols::line::NORMAL.vertical_left,
+                            ..symbols::border::PLAIN
+                        })
                         .title_style(Style::default().fg(Color::LightRed).add_modifier(Modifier::BOLD))
                         .title(" 󱐋 Fuel Cells ")
                 );
@@ -98,21 +133,29 @@ impl Interface {
                 .style(default_style)
                 .block(
                     Block::default()
-                        .borders(Borders::LEFT | Borders::TOP)
+                        .borders(Borders::LEFT | Borders::TOP | Borders::RIGHT)
+                        .border_set(symbols::border::Set {
+                            top_left: symbols::line::NORMAL.vertical_right,
+                            top_right: symbols::line::NORMAL.vertical_left,
+                            ..symbols::border::PLAIN
+                        })
                         .title_style(Style::default().fg(Color::LightYellow).add_modifier(Modifier::BOLD))
                         .title(" 󱍗 Tank ")
                 );
 
-                let boat_widget = Paragraph::new(format!("Temperature:{:>8.2}", 142.1283))
+                let messages_widget = Paragraph::new(format!("No message"))
                 .style(default_style)
                 .block(
                     Block::default()
-                        .borders(Borders::LEFT | Borders::TOP)
-                        .title_style(Style::default().fg(Color::LightMagenta).add_modifier(Modifier::BOLD))
-                        .title(" 󰻈 Ship ")
+                        .borders(Borders::ALL)
+                        .border_set(symbols::border::Set {
+                            top_left: symbols::line::NORMAL.vertical_right,
+                            top_right: symbols::line::NORMAL.vertical_left,
+                            ..symbols::border::PLAIN
+                        })
+                        .title_style(Style::default().fg(Color::White).add_modifier(Modifier::BOLD))
+                        .title("  Messages ")
                 );
-
-
 
                 /////////////
                 // Layouts
@@ -120,125 +163,49 @@ impl Interface {
 
                 let main_layout = Layout::default()
                     .direction(Direction::Vertical)
-                    .constraints([Constraint::Length(14), Constraint::Length(4)].as_ref())
+                    .constraints([Constraint::Length(10), Constraint::Length(4)].as_ref())
                     .split(frame.size());
 
                 let columns_layout = Layout::default()
                     .direction(Direction::Horizontal)
-                    .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+                    .constraints([
+                            Constraint::Length(30),
+                            Constraint::Length(31)
+                        ].as_ref())
                     .split(main_layout[0]);
 
                 let left_column_layout = Layout::default()
                     .direction(Direction::Vertical)
-                    .constraints(
-                        [
-                            Constraint::Length(5),
-                            Constraint::Length(4),
-                        ]
-                        .as_ref(),
-                    )
+                    .constraints([
+                        Constraint::Length(2),
+                        Constraint::Length(2),
+                        Constraint::Length(6),
+                    ].as_ref(),)
                     .split(columns_layout[0]);
 
                 let right_column_layout = Layout::default()
                     .direction(Direction::Vertical)
-                    .constraints(
-                        [
-                            Constraint::Length(3),
-                            Constraint::Length(4),
-                            Constraint::Length(2),
-                        ]
-                        .as_ref(),
-                    )
+                    .constraints([
+                        Constraint::Length(4),
+                        Constraint::Length(4),
+                        Constraint::Length(2),
+                    ].as_ref(),)
                     .split(columns_layout[1]);
 
-                // // left column widgets
-                // frame.render_widget(
-                //     Block::default()
-                //         .borders(Borders::LEFT | Borders::TOP)
-                //         .title("Battery"),
-                //     left_column_layout[0],
-                // );
-                // frame.render_widget(
-                //     Block::default()
-                //         .borders(Borders::LEFT | Borders::TOP)
-                //         .border_set(symbols::border::Set {
-                //             top_left: symbols::line::NORMAL.vertical_right,
-                //             ..symbols::border::PLAIN
-                //         })
-                //         .title("Pressure Sensors"),
-                //     left_column_layout[1],
-                // );
-                // frame.render_widget(
-                //     Block::default()
-                //         .borders(Borders::LEFT | Borders::TOP)
-                //         .border_set(symbols::border::Set {
-                //             top_left: symbols::line::NORMAL.vertical_right,
-                //             ..symbols::border::PLAIN
-                //         })
-                //         .title("Hydrogen"),
-                //     left_column_layout[2],
-                // );
+                let messages_layout = Layout::default()
+                    .direction(Direction::Horizontal)
+                    .constraints([
+                        Constraint::Length(61),
+                    ].as_ref(),)
+                    .split(main_layout[1]);
 
-                frame.render_widget(battery_widget, left_column_layout[0]);
-                frame.render_widget(hydrogen_widget, left_column_layout[1]);
-                frame.render_widget(boat_widget, right_column_layout[0]);
+                frame.render_widget(speed_widget, left_column_layout[0]);
+                frame.render_widget(efficiency_widget, left_column_layout[1]);
+                frame.render_widget(battery_widget, left_column_layout[2]);
+                frame.render_widget(hydrogen_widget, right_column_layout[0]);
                 frame.render_widget(fuel_cells_widget, right_column_layout[1]);
                 frame.render_widget(tank_widget, right_column_layout[2]);
-
-                // right column widgets
-                // frame.render_widget(
-                //     Block::default()
-                //         .borders(Borders::LEFT | Borders::TOP | Borders::RIGHT)
-                //         .border_set(symbols::border::Set {
-                //             top_left: symbols::line::NORMAL.horizontal_down,
-                //             ..symbols::border::PLAIN
-                //         })
-                //         .title("Efficiency"),
-                //     right_column_layout[0],
-                // );
-                // frame.render_widget(
-                //     Block::default()
-                //         .borders(Borders::LEFT | Borders::TOP | Borders::RIGHT)
-                //         .border_set(symbols::border::Set {
-                //             top_left: symbols::line::NORMAL.horizontal_down,
-                //             top_right: symbols::line::NORMAL.vertical_left,
-                //             ..symbols::border::PLAIN
-                //         })
-                //         .title("Fuel Cells"),
-                //     right_column_layout[1],
-                // );
-                // frame.render_widget(
-                //     Block::default()
-                //         .borders(Borders::LEFT | Borders::TOP | Borders::RIGHT)
-                //         .title("Speed"),
-                //     right_column_layout[2],
-                // );
-                // frame.render_widget(
-                //     Block::default()
-                //         .borders(Borders::LEFT | Borders::TOP | Borders::RIGHT)
-                //         .title("Tank"),
-                //     right_column_layout[3],
-                // );
-
-                // messages
-                frame.render_widget(
-                    Block::default()
-                        .borders(Borders::LEFT | Borders::TOP | Borders::RIGHT | Borders::BOTTOM)
-                        .title("Messages"),
-                    main_layout[1],
-                );
-
-                // // 1. batteries
-                // let text = format!("BAT✈ T°: {:>6.2} \nBAT T°: {:>6.2}", 14.412, 140.3);
-                // frame.render_widget(
-                //     Paragraph::new(text).white().on_blue(),
-                //     Rect {
-                //         x: 0,
-                //         y: 0,
-                //         width: 100,
-                //         height: 20,
-                //     },
-                // );
+                frame.render_widget(messages_widget, messages_layout[0]);
             })
             .unwrap();
     }
